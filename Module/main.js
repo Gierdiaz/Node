bf5d8ec92d9c475cb83236e8ab877cbbab455d76
Gierdiaz <gierdiaz@hotmail.com>
2023-09-05 09:47:04 -0300

    Módulos e Require

---
+++ Module/main.js
@@ -0,0 +1,5 @@
+//módulos e require
+console.log('Início');
+
+let data = require('./module');
+console.log(data.city[2]);
---
+++ Module/module.js
@@ -0,0 +1,8 @@
+const animals = ['Elephant','Giraffe','Dolphin','Bear','Vulture'];
+
+const city = ['Rio de Janeiro', 'São Paulo', 'Minas Gerais', 'Santa Catarina', 'Bahia'];
+
+module.exports = {
+    animals: animals,
+    city: city
+};
